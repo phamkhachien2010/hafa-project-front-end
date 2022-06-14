dca9858d8729739c72f92af6eb2938be359c22c9
phamkhachien2010 <phamkhachien17@gmail.com>
2022-06-14 14:56:39 +0700

    create homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,7 +1,34 @@
+import { Card } from 'antd'
 import React from 'react'
 
 export default function Homepage() {
   return (
-    <div>Homepage</div>
+    <div className='w-4/5 m-auto'>
+      <div className="grid grid-cols-2 gap-2">
+        <div className='mb-5'>
+          <Card title="Card title" >
+            Card content
+          </Card>
+        </div>
+
+        <div>
+          <Card title="Card title" >
+            Card content
+          </Card>
+        </div>
+
+        <div>
+          <Card title="Card title" >
+            Card content
+          </Card>
+        </div>
+
+        <div>
+          <Card title="Card title" >
+            Card content
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
